82a95523132e8b82ac7fefc3000fa8fdad44a05a
Felipe Ribeiro <125563054+FelipeRibeiro12@users.noreply.github.com>
2023-06-11 20:33:16 -0300

    upload nao havia sido feito corretamente

--- RedeSocial.java
+++ RedeSocial.java
@@ -3,26 +3,18 @@ public abstract class RedeSocial{
     protected String senha;
     protected int numAmigos;
 
-    //construtor superclass
+    //construtor superclasse
     public RedeSocial(String senha, int numAmigos){
         this.senha = senha;
         this.numAmigos = numAmigos;
     }
 
-    public abstract void postarFoto();{
-       System.out.println("Postou uma foto");
-    }
+    public abstract void postarFoto();
 
-    public abstract void postarVideo();{
-        System.out.println("Postou um video");
-    }
+    public abstract void postarVideo();
 
-    public abstract void postarComentario();{
-        System.out.println("Postou um comentário");
-    }
+    public abstract void postarComentario();
 
-    public abstract void curtirPublicacao();{
-        System.out.println("Curtiu uma publicação");
-    }
+    public abstract void curtirPublicacao();
 
 }
